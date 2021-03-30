8a1e29f63551b1cfc9dc5415b7eae39d7144a140
gabrielgdhl <gabrielroquesousa@gmail.com>
2021-03-30 19:54:49 -0400

    adicionei animações

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function start() {
+    $("#inicio").hide();
+
+    $("#bg-game").append("<div id='jogador' class='anima1'></div>");
+    $("#bg-game").append("<div id='inimigo1' class='anima-inimigo1'></div>");
+    $("#bg-game").append("<div id='inimigo2'></div>");
+    $("#bg-game").append("<div id='amigo' class='anima-amigo'></div>");
+    
+}
